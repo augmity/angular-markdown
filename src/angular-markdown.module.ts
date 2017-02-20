cabcbb664ab2885fd73c9a8028e3333370407ef6
Tomek Michniewicz <michniewicz.tomasz@gmail.com>
2017-02-19 17:49:37 -0800

    lib updates

--- src/angular-markdown.module.ts
+++ src/angular-markdown.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { MarkdownEditorComponent } from './markdown-editor.component';
 import { MarkdownViewerComponent } from './markdown-viewer.component';
 
 @NgModule({
+  imports: [
+    FormsModule
+  ],
   declarations: [
     MarkdownEditorComponent,
     MarkdownViewerComponent
